fcf540a96a48f0665d5f4668234d3aac5ec9372e
DEXIZA <dexizaia@gmail.com>
2021-06-07 14:12:12 +0900

    投稿ボタンをクリックしたときにイベント発火するように設定しましょう
    投稿ボタンがクリックされることをトリガーにして、非同期通信でメモが投稿されるように設定します。
    
    「投稿ボタンがクリックされたこと」を認識するために、submit.addEventListenerと記述します。そうすることによって、先ほど定義したsubmit（投稿ボタン）に対するイベントを認識することができます。
    
    今回は「クリックされた」というイベントを認識したいため、addEventListenerメソッドの第一引数にはclickイベントを指定します。そして、第二引数に実行したい処理を記述しましょう。
    
    具体的な処理内容は後ほど記述するので、今は() => {}としておきましょう。
    
    memo.jsを以下のように編集しましょう。

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,11 @@
 function post (){
   const submit = document.getElementById("submit");
   // index.html.erbより、getElementByIdメソッドで取得した投稿ボタンの要素を変数submitに格納しています。
+  submit.addEventListener("click", () => {
+  });
+  // 「投稿ボタンがクリックされたこと」を認識するために、submit.addEventListenerと記述します。
+  // 今回は「クリックされた」というイベントを認識したいため、addEventListenerメソッドの第一引数にはclickイベントを指定します。そして、第二引数に実行したい処理を記述しましょう。
+  // 具体的な処理内容は後ほど記述するので、今は() => {}としておきましょう。
 };
 // ページが読み込まれた時に発動する
 
