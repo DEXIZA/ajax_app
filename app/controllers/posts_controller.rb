22cb724fb4f528c576a0bd549afaef1ce6b9c463
DEXIZA <dexizaia@gmail.com>
2021-06-07 12:44:44 +0900

    投稿後にトップページにリダイレクトされるようにしましょう
    メモを保存した後にトップページへリダイレクトされるようにしましょう。
    また、newアクションは不要なので、コメントアウトしましょう。
    posts_controller.rbを以下のように編集しましょう。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,12 @@ class PostsController < ApplicationController
     # orderメソッドで降順にしている
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index  
+    # 投稿した後にリダレクトするようにする
   end
 end
