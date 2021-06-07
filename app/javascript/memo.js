be4076177b5db50c016eb169429d274a09d0f8d4
DEXIZA <dexizaia@gmail.com>
2021-06-07 14:09:01 +0900

    投稿ボタンの要素を取得
    getElementByIdメソッドを用いて、クリックの対象となる投稿ボタンの要素を取得します。
    まずは、index.html.erbを開いて、投稿ボタンにsubmitというidが付与されていることを確認しましょう。
    
      <%= form.submit '投稿する' , id: "submit" %>
    
    確認ができたら、上記の4行目の値をJavaScriptで取得できるようにします。
    memo.jsを以下のように編集しましょう。
    
    getElementByIdメソッドで取得した投稿ボタンの要素を変数submitに格納しています。

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,8 @@
 function post (){
-  console.log("イベント発火")
+  const submit = document.getElementById("submit");
+  // index.html.erbより、getElementByIdメソッドで取得した投稿ボタンの要素を変数submitに格納しています。
 };
+// ページが読み込まれた時に発動する
 
  
  window.addEventListener('load', post);
