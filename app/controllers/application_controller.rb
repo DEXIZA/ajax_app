cdb776c4c38b60ec7a74d963808728bd2c4faf03
DEXIZA <dexizaia@gmail.com>
2021-06-10 13:49:42 +0900

    ユーザー認証を導入しよう
    現在のAjaxAppは、世界中の誰でもアクセスできる状態にあります。閲覧できるユーザーを制限するためにBasic認証を導入しましょう。
    
    Basicベーシック認証
    Basic認証とは、HTTP通信の規格に備え付けられている、ユーザー認証の仕組みのことです。
    サーバーとの通信が可能なユーザーとパスワードをあらかじめ設定しておき、それに一致したユーザーのみが、Webアプリケーションを利用できるようにします。
    
    Basic認証によるログインの要求は、すべてのコントローラーで行いたいです。そこで、Basic認証の処理をapplication_controller.rbのprivate以下にメソッドとして定義し、before_actionで呼び出しましょう。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
+
+
+# authenticate_or_request_with_http_basicオウセンティケイト オア リクエスト ウィズ エイチティティーピー ベーシックメソッド
+  # RailsアプリケーションでBasic認証を実装するために使用する、Railsのメソッドです。
+  # ブロックを開き、ブロック内部でusernameとpasswordを設定することでBasic認証を利用できます。
+    #   # 'admin'というユーザー名と、'password'というパスワードでBasic認証できるように設定
+    #   authenticate_or_request_with_http_basic do |username, password|
+    #     username == 'admin' && password == 'password'
+    #   end
